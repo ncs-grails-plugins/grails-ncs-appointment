ab2e5e0bb05b2d97114202854ce9280d685e8e3d
Aaron J. Zirbes <aaron.zirbes@gmail.com>
2012-03-27 13:39:07 -0500

    bumping plugin version for new release

--- NcsAppointmentGrailsPlugin.groovy
+++ NcsAppointmentGrailsPlugin.groovy
@@ -1,10 +1,10 @@
 class NcsAppointmentGrailsPlugin {
     // the plugin version
-    def version = "0.2"
+    def version = "0.3"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.3.5 > *"
     // the other plugins this plugin depends on
-    def dependsOn = [ ncsTracking : "0.3 > *", ncsPeople : "0.1 > *" ]
+    def dependsOn = [ ncsTracking : "3.2.6 > *", ncsPeople : "1.0 > *" ]
     // resources that are excluded from plugin packaging
     def pluginExcludes = [
             "grails-app/views/error.gsp",
